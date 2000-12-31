eb9d9a18c04ba687f9200ed2132cbec8efdd6f30
newmana <>
2000-12-31 08:33:04 +0000

    Fixed so that it will now compile

--- RCOSjava/MessageSystem/Messages/Universal/ProcessToTerminalAllocation.java
+++ RCOSjava/MessageSystem/Messages/Universal/ProcessToTerminalAllocation.java
@@ -1,6 +1,6 @@
 //*******************************************************************/
 // FILE     : ProcessToTerminalAllocationMessage.java
-// PURPOSE  : Used to assign a terminal to the next 
+// PURPOSE  : Used to assign a terminal to the next
 //            waiting process.
 // AUTHOR   : David Jones
 // MODIFIED : Andrew Newman
@@ -28,29 +28,13 @@ public class ProcessToTerminalAllocation extends UniversalMessageAdapter
     super(theSource);
     this.sTerminalID = sTerminalID;
     this.iPID = iPID;
-  }  
+  }
 
   public void doMessage(ProcessScheduler theElement)
-  {           
-      // Try and retrieve the next process in the ZombieQ          
-      RCOSProcess rpNewProc = (RCOSProcess) 
-        theElement.removeFromZombieCreatedQ(iPID);
-        
-      // If a process is retrieve allocate terminal and move
-      // to Ready Q.
-      if (rpNewProc != null)
-      {
-        rpNewProc.setTerminalID(sTerminalID);
-        rpNewProc.setStatus(RCOSProcess.READY); 
-        theElement.insertIntoReadyQ(rpNewProc);
-      }
-      
-      // If there is no current process running schedule
-      // process to be run.
-      if (theElement.getCurrentProcess() == null)
-        theElement.schedule();
+  {
+    theElement.processAllocatedTerminal(iPID, sTerminalID);
   }
-  
+
   public void doMessage(ProcessSchedulerAnimator theElement)
   {
     theElement.zombieToReady(iPID);
