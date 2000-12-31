e64858576e7122234163efa1ad8eb37fc133c07f
newmana <>
2000-12-31 04:57:20 +0000

    for message recording

---
+++ RCOSjava/MessageSystem/PostOffices/OS/OSMessageRecorder.java
@@ -0,0 +1,23 @@
+package MessageSystem.PostOffices.OS;
+
+import MessageSystem.PostOffices.SimpleMessageHandler;
+import MessageSystem.Messages.OS.OSMessageAdapter;
+import MessageSystem.Messages.Universal.UniversalMessageAdapter;
+import MessageSystem.Messages.MessageAdapter;
+import MessageSystem.PostOffices.PostOffice;
+import MessageSystem.Messages.AddHandler;
+
+public class OSMessageRecorder extends OSMessageHandler
+{
+  public OSMessageRecorder()
+  {
+  }
+
+  public void processMessage(OSMessageAdapter mMessage)
+  {
+  }
+
+  public void processMessage(UniversalMessageAdapter mMessage)
+  {
+  }
+}
