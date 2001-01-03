4391d85a1189370c3b9669a91490b38364b10582
newmana <>
2001-01-03 11:10:39 +0000

    improve formatting and code style

--- RCOSjava/Hardware/Disk/CPM14/CPM14Floppy.java
+++ RCOSjava/Hardware/Disk/CPM14/CPM14Floppy.java
@@ -3,9 +3,9 @@
 // PACKAGE  : Hardware.Disk.CPM14
 // PURPOSE  : A simulated CPM14 floppy disk.
 // AUTHOR   : Andrew Newman
-// MODIFIED : 
+// MODIFIED :
 // HISTORY  : 10/08/99 Created
-//            
+//
 //*************************************************************************//
 
 package Hardware.Disk.CPM14;
@@ -14,9 +14,9 @@ import Hardware.Disk.SimpleDisk;
 
 public class CPM14Floppy extends SimpleDisk
 {
-	public CPM14Floppy(int iNewTracks, int iNewSectorsPerTrack, 
-		int iBytesPerSector)
-	{
-		super(iNewTracks, iNewSectorsPerTrack, iBytesPerSector, 100, 1000, 300);
-	}
+  public CPM14Floppy(int newTracks, int newSectorsPerTrack,
+    int bytesPerSector)
+  {
+    super(newTracks, newSectorsPerTrack, bytesPerSector, 100, 1000, 300);
+  }
 }
--- RCOSjava/Hardware/Disk/CPM14/CPM14Harddrive.java
+++ RCOSjava/Hardware/Disk/CPM14/CPM14Harddrive.java
@@ -3,9 +3,9 @@
 // PACKAGE  : Hardware.Disk.CPM14
 // PURPOSE  : A simulated CPM14 hard drive disk.
 // AUTHOR   : Andrew Newman
-// MODIFIED : 
+// MODIFIED :
 // HISTORY  : 10/08/99 Created
-//            
+//
 //*************************************************************************//
 
 package Hardware.Disk.CPM14;
@@ -14,9 +14,9 @@ import Hardware.Disk.SimpleDisk;
 
 public class CPM14Harddrive extends SimpleDisk
 {
-	public CPM14Harddrive(int iNewTracks, int iNewSectorsPerTrack, 
-		int iBytesPerSector)
-	{
-		super(iNewTracks, iNewSectorsPerTrack, iBytesPerSector, (float) 0.3, 20, 3600);
-	}
+  public CPM14Harddrive(int newTracks, int newSectorsPerTrack,
+    int bytesPerSector)
+  {
+    super(newTracks, newSectorsPerTrack, bytesPerSector, (float) 0.3, 20, 3600);
+  }
 }
