24754808b8b7e8b70b3ff9b6c8a33a6c5d335a0a
newmana <>
2001-04-14 04:30:46 +0000

    Fixed for new packaging.

---
+++ bin/runme.bat
@@ -0,0 +1,3 @@
+@echo off
+java -classpath %CLASSPATH%;../deploy/RCOSjava.jar net.sourceforge.rcosjava.pll2.FileServer ..\deploy\pll2\executable ..\deploy\pll2\recorded 4242
+
---
+++ bin/runme.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+java -classpath %CLASSPATH%:../deploy/RCOSjava.jar net.sourceforge.rcosjava.pll2.FileServer ../deploy/pll2/executable ../deploy/pll2/recorded 4242
+
