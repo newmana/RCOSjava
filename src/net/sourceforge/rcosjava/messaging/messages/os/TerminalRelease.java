25e3522a2e63d85027931950b4f5b13dccf6a7b0
newmana <>
2001-04-16 04:11:26 +0000

    Fix for terminal manager bug (416239)

--- src/net/sourceforge/rcosjava/messaging/messages/os/TerminalRelease.java
+++ src/net/sourceforge/rcosjava/messaging/messages/os/TerminalRelease.java
@@ -1,38 +1,40 @@
-//***************************************************************************
-// FILE    : TerminalReleaseMessage.java
-// PACKAGE : Terminal
-// PURPOSE :
-// AUTHOR  : Andrew Newman
-// MODIFIED:
-// HISTORY : 28/03/96  Created
-//
-//
-//***************************************************************************
-
 package net.sourceforge.rcosjava.messaging.messages.os;
 
 import net.sourceforge.rcosjava.software.terminal.TerminalManager;
 import net.sourceforge.rcosjava.messaging.postoffices.os.OSMessageHandler;
+import net.sourceforge.rcosjava.messaging.messages.universal.TerminalOff;
 
+/**
+ * Called by the Process Scheduler to indicate that the process has complete and
+ * that the terminal should be released for reuse.
+ * <P>
+ * @author Andrew Newman.
+ * @version 1.00 $Date$
+ * @created 1st January 1998
+ */
 public class TerminalRelease extends OSMessageAdapter
 {
-  private String sTerminalID;
+  private String terminalId;
 
   public TerminalRelease(OSMessageHandler theSource,
-    String sNewTerminalID)
+    String newTerminalId)
   {
     super(theSource);
-    sTerminalID = sNewTerminalID;
+    terminalId = newTerminalId;
   }
 
-  public void setValues(String sNewTerminalID)
+  public void setValues(String newTerminalId)
   {
-    sTerminalID = sNewTerminalID;
+    terminalId = newTerminalId;
   }
 
+  /**
+   * Calls removeTerminal on the Terminal Manager with the stored terminal id.
+   *
+   * @param theElement the Terminal Manager to do the work on.
+   */
   public void doMessage(TerminalManager theElement)
   {
-    theElement.removeTerminal(sTerminalID);
+    theElement.releaseTerminal(terminalId);
   }
 }
-
