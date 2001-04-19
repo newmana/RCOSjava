5fe7b9f0d658feccc968d0adea25c39d7c8b2183
newmana <>
2001-04-19 20:55:18 +0000

    Removed unneeded code.  Turned on representation of reading/writing, etc and process allocation.

--- src/net/sourceforge/rcosjava/software/animator/ipc/MemoryGraphic.java
+++ src/net/sourceforge/rcosjava/software/animator/ipc/MemoryGraphic.java
@@ -17,92 +17,70 @@ import net.sourceforge.rcosjava.software.memory.MemoryReturn;
 
 public class MemoryGraphic extends Canvas
 {
-  public static final Color cReading = Color.blue;
-  public static final Color cWriting = Color.red;
-  public static final Color cAllocated = Color.gray;
-  public static final Color cUnAllocColour = Color.black;
-  public static final Color cUnallocated = Color.black;
+  public static final Color readingColour = Color.blue;
+  public static final Color writingColour = Color.red;
+  public static final Color allocatedColour = Color.gray;
+  public static final Color unallocatedColour = Color.black;
 
-  private int iID;
-  private byte bType;
-  private int iSize;
-  private int iDefaultWidth;
-  private int iDefaultHeight;
-  private Color cCurrent = Color.gray;
-  private Color cTextColour = Color.yellow;
-  private Image iMyImage;
-  private Font fTheFont = new Font("TimesRoman", Font.PLAIN, 12);
-  private boolean bAllocated = false;
-  private String sText;
+  private int id;
+  private byte memoryType;
+  private int memorySize;
+  private int defaultWidth;
+  private int defaultHeight;
+  private Color currentColour = Color.gray;
+  private Color textColour = Color.yellow;
+  private Image memoryImage;
+  private Font textFont = new Font("TimesRoman", Font.PLAIN, 12);
+  private boolean allocated = false;
+  private String text;
 
-  public MemoryGraphic(Image aImage)
+  public MemoryGraphic(Image newImage)
   {
     super();
-    sText = "";
-    iMyImage = aImage;
-    iDefaultWidth = iMyImage.getWidth(getParent());
-    iDefaultHeight = iMyImage.getHeight(getParent());
-    this.setSize(iDefaultWidth,iDefaultHeight);
-    repaint();
+    text = "";
+    memoryImage = newImage;
+    defaultWidth = memoryImage.getWidth(getParent());
+    defaultHeight = memoryImage.getHeight(getParent());
+    this.setSize(defaultWidth,defaultHeight);
+    //repaint();
   }
 
-  public boolean isMemory(int iMemID, byte bMemType)
+  public boolean isMemory(int memoryId, byte memoryType)
   {
-    return ((iID == iMemID) && (bMemType == bMemType));
+    return ((id == memoryId) && (memoryType == memoryType));
   }
 
-  public void setCurrentColour(Color cNewColour)
+  public void setCurrentColour(Color newColour)
   {
-    cCurrent = cNewColour;
+    currentColour = newColour;
     this.repaint();
   }
 
-  public void setAllocated(MemoryReturn aMemret)
+  public void setAllocated(MemoryReturn newMemoryReturn)
   {
-    bAllocated = true;
+    setValues(newMemoryReturn);
+    allocated = true;
   }
 
   public void setDeallocated()
   {
-    sText = "";
-    bAllocated = false;
-    iID = 0;
-    bType = 0;
-    iSize = 0;
-    this.repaint();
+    text = "";
+    allocated = false;
+    id = 0;
+    memoryType = 0;
+    memorySize = 0;
   }
 
   private void setValues(MemoryReturn aMemret)
   {
-    sText = "P" + aMemret.getPID();
-    iID = aMemret.getPID();
-    bType = aMemret.getMemoryType();
-    iSize = aMemret.getSize();
+    text = "P" + aMemret.getPID();
+    id = aMemret.getPID();
+    memoryType = aMemret.getMemoryType();
+    memorySize = aMemret.getSize();
     this.repaint();
   }
 
-  public Dimension getMinimumSize()
-  {
-    return setPreferredSize();
-  }
-
-  public Dimension setPreferredSize()
-  {
-    return new Dimension (iDefaultWidth, iDefaultHeight);
-  }
-
-  public synchronized void addNotify()
-  {
-    repaint();
-    super.addNotify();
-  }
-
-  public void paint (Graphics g)
-  {
-    update(g);
-  }
-
-  public void update (Graphics g)
+  public void paint(Graphics g)
   {
     FontMetrics fm = g.getFontMetrics();
 
@@ -110,27 +88,27 @@ public class MemoryGraphic extends Canvas
     //center text (the program gets the font from
     //the current Graphics context).
 
-    int iX = (iDefaultWidth/2) - (fm.stringWidth(sText)/2);
-    int iY = (iDefaultHeight/2) + (fm.getAscent()/2);
+    int iX = (defaultWidth/2) - (fm.stringWidth(text)/2);
+    int iY = (defaultHeight/2) + (fm.getAscent()/2);
 
-    if (bAllocated)
+    if (allocated)
     {
-      g.setColor(cCurrent);
+      g.setColor(currentColour);
     }
     else
     {
-      g.setColor(cUnAllocColour);
+      g.setColor(unallocatedColour);
     }
 
-    g.drawImage(iMyImage, 0, 0, this);
+    g.drawImage(memoryImage, 0, 0, this);
 
-    g.fillRect(((int) iDefaultWidth/2)-10,((int) iDefaultHeight/2)-10,20,20);
+    g.fillRect(((int) defaultWidth/2)-10,((int) defaultHeight/2)-10,20,20);
 
     //When drawing text to the screen the x,y co-ordinate
     //is of the baseline of the text.
 
-    g.setColor(cTextColour);
-    g.setFont(fTheFont);
-    g.drawString(sText, iX, iY);
+    g.setColor(textColour);
+    g.setFont(textFont);
+    g.drawString(text, iX, iY);
   }
 }
