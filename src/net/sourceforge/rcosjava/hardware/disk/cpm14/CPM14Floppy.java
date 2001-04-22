64ea5527563ad1fd5f694fd7ec63774eae18a4c7
newmana <>
2001-04-22 03:34:09 +0000

    Added.

---
+++ src/net/sourceforge/rcosjava/hardware/disk/cpm14/CPM14Floppy.java
@@ -0,0 +1,26 @@
+package net.sourceforge.rcosjava.hardware.disk.cpm14;
+
+import net.sourceforge.rcosjava.hardware.disk.SimpleDisk;
+
+/**
+ * A sumulated CPM14 floppy disk.
+ * <P>
+ * @see net.sourceforge.rcosjava.hardware.disk.SimpleDisk
+ * @see net.sourceforge.rcosjava.hardware.disk.Disk
+ * @author Andrew Newman.
+ * @version 1.00 $Date$
+ * @created 10th of August 1999
+ */
+public class CPM14Floppy extends SimpleDisk
+{
+  /**
+   * Create a floppy with the given number of tracks, sectors and bytes per
+   * sector.  The disk latency is 100ms, the start up time is 1000ms and the RPM
+   * is 300.
+   */
+  public CPM14Floppy(int newTracks, int newSectorsPerTrack,
+    int bytesPerSector)
+  {
+    super(newTracks, newSectorsPerTrack, bytesPerSector, 100, 1000, 300);
+  }
+}
---
+++ src/net/sourceforge/rcosjava/hardware/disk/cpm14/CPM14HardDrive.java
@@ -0,0 +1,26 @@
+package net.sourceforge.rcosjava.hardware.disk.cpm14;
+
+import net.sourceforge.rcosjava.hardware.disk.SimpleDisk;
+
+/**
+ * A sumulated CPM14 hard disk drive.
+ * <P>
+ * @see net.sourceforge.rcosjava.hardware.disk.SimpleDisk
+ * @see net.sourceforge.rcosjava.hardware.disk.Disk
+ * @author Andrew Newman.
+ * @version 1.00 $Date$
+ * @created 10th of August 1999
+ */
+public class CPM14HardDrive extends SimpleDisk
+{
+  /**
+   * Create a floppy with the given number of tracks, sectors and bytes per
+   * sector.  The disk latency is 0.3ms, the start up time is 20ms and the RPM
+   * is 3600.
+   */
+  public CPM14HardDrive(int newTracks, int newSectorsPerTrack,
+    int bytesPerSector)
+  {
+    super(newTracks, newSectorsPerTrack, bytesPerSector, (float) 0.3, 20, 3600);
+  }
+}
