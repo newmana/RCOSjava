64202928e0c10c089dd95601a972b878c80cc7ef
newmana <>
2001-11-14 11:55:08 +0000

    Added more if statements for int values (and it works)

--- src/net/sourceforge/rcosjava/compiler/test2.c
+++ src/net/sourceforge/rcosjava/compiler/test2.c
@@ -8,8 +8,28 @@ void main()
 
   if ((global > 0))
   {
-    printf("Hello world\n");
+    printf("Global greater than 0\n");
     
+  }
+
+  if ((global == 0))
+  {
+    printf("Global equal to 0\n");
+  }
+
+  if ((global == 1))
+  {
+    printf("Global equal to 1\n");
+  }
+
+  if ((global < 1))
+  {
+    printf("Global less than 1\n");
+  }
+
+  if ((global < 2))
+  {
+    printf("Global less than 2\n");
   }
 }
 
