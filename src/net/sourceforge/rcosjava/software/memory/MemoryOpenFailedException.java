c70b21a4b37d4df236baeb01e63f52a5dae72819
newmana <>
2002-05-14 20:09:41 +0000

    Should've been removed ages ago.

--- src/net/sourceforge/rcosjava/software/memory/MemoryOpenFailedException.java
+++
@@ -1,8 +0,0 @@
-package net.sourceforge.rcosjava.software.memory;
-
-/**
- * The exception thrown when the allocation of memory fails.
- */
-public class MemoryOpenFailedException extends Exception
-{
-}
