57a1cd5b80c2599148f087329cfdca737bc0d6cf
newmana <>
2002-05-19 11:44:38 +0000

    Added test for math functions.

---
+++ src/c/test8.c
@@ -0,0 +1,28 @@
+/**
+ * Test simple mathematical functions.
+ **/
+
+int main()
+{
+  int result;
+
+  result = (27 / 3);
+  printf("27 / 3 = ");
+  printf("%i", result);
+  printf("\n");
+
+  result = (3 * 3);
+  printf("3 * 3 = ");
+  printf("%i", result);
+  printf("\n");
+
+  result = (27 - 15);
+  printf("27 - 15 = ");
+  printf("%i", result);
+  printf("\n");
+
+  result = (23 + 19);
+  printf("23 + 19 = ");
+  printf("%i", result);
+  printf("\n");
+}
