1f01a56fc9155f4bb8f25d43500b31d19a65e9fb
newmana <>
2002-07-23 04:40:15 +0000

    Commented out for now.

--- src/java/org/rcosjava/messaging/messages/animator/ShowCPU.java
+++ src/java/org/rcosjava/messaging/messages/animator/ShowCPU.java
@@ -1,1 +1,34 @@
-package org.rcosjava.messaging.messages.animator;import org.rcosjava.messaging.postoffices.animator.AnimatorMessageHandler;import org.rcosjava.software.animator.cpu.CPUAnimator;/** * Display the CPU in the CPU animator. * * @author Andrew Newman. * @created 1st July 1996 * @version 1.00 $Date$ */public class ShowCPU extends AnimatorMessageAdapter{  /**   * Create the message sender.   *   * @param theSource Description of Parameter   */  public ShowCPU(AnimatorMessageHandler theSource)  {    super(theSource);  }  /**   * Call the CPUAnimator's showCPU method call.   *   * @param theElement Description of Parameter   */  public void doMessage(CPUAnimator theElement)  {    theElement.showCPU();  }}
+package org.rcosjava.messaging.messages.animator;
+import org.rcosjava.messaging.postoffices.animator.AnimatorMessageHandler;
+import org.rcosjava.software.animator.cpu.CPUAnimator;
+
+/**
+ * Display the CPU in the CPU animator.
+ *
+ * @author Andrew Newman.
+ * @created 1st July 1996
+ * @version 1.00 $Date$
+ */
+public class ShowCPU extends AnimatorMessageAdapter
+{
+  /**
+   * Create the message sender.
+   *
+   * @param theSource Description of Parameter
+   */
+  public ShowCPU(AnimatorMessageHandler theSource)
+  {
+    super(theSource);
+  }
+
+  /**
+   * Call the CPUAnimator's showCPU method call.
+   *
+   * @param theElement Description of Parameter
+   */
+  public void doMessage(CPUAnimator theElement)
+  {
+//    theElement.showCPU();
+  }
+}
+
