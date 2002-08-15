2e821e00b24f275845b84aa12e6c2e18c3c9c9b4
newmana <>
2002-08-15 12:06:52 +0000

    Updated javadoc.

--- src/java/org/rcosjava/messaging/messages/os/KeyPress.java
+++ src/java/org/rcosjava/messaging/messages/os/KeyPress.java
@@ -1,1 +1,53 @@
-package org.rcosjava.messaging.messages.os;import org.rcosjava.hardware.cpu.Interrupt;import org.rcosjava.messaging.postoffices.os.OSMessageHandler;import org.rcosjava.software.terminal.SoftwareTerminal;/** * Sent by the terminal interrupt handler when the kernal gets a interrupt. <P> * * * * @author Andrew Newman. * @created 24th of January 1996 * @version 1.00 $Date$ */public class KeyPress extends OSMessageAdapter{  /**   * The generated interrupt.   */  private Interrupt interrupt;  /**   * Constructor for the KeyPress object   *   * @param theSource Description of Parameter   * @param newInterrupt Description of Parameter   */  public KeyPress(OSMessageHandler theSource,      Interrupt newInterrupt)  {    super(theSource);    interrupt = newInterrupt;  }  /**   * Description of the Method   *   * @param theElement Description of Parameter   */  public void doMessage(SoftwareTerminal theElement)  {    theElement.keyPress();  }  /**   * Sets the Interrupt attribute of the KeyPress object   *   * @param intNewInterrupt The new Interrupt value   */  private void setInterrupt(Interrupt intNewInterrupt)  {    interrupt = intNewInterrupt;  }}
+package org.rcosjava.messaging.messages.os;
+import org.rcosjava.hardware.cpu.Interrupt;
+import org.rcosjava.messaging.postoffices.os.OSMessageHandler;
+import org.rcosjava.software.terminal.SoftwareTerminal;
+
+/**
+ * Sent by the terminal interrupt handler when the kernal gets a interrupt.
+ * <P>
+ * @author Andrew Newman.
+ * @created 24th of January 1996
+ * @version 1.00 $Date$
+ */
+public class KeyPress extends OSMessageAdapter
+{
+  /**
+   * The generated interrupt.
+   */
+  private Interrupt interrupt;
+
+  /**
+   * Constructor for the KeyPress object
+   *
+   * @param theSource Description of Parameter
+   * @param newInterrupt Description of Parameter
+   */
+  public KeyPress(OSMessageHandler theSource,
+      Interrupt newInterrupt)
+  {
+    super(theSource);
+    interrupt = newInterrupt;
+  }
+
+  /**
+   * Call key press on the given element.
+   *
+   * @param theElement software terminal to call method on.
+   */
+  public void doMessage(SoftwareTerminal theElement)
+  {
+    theElement.keyPress();
+  }
+
+  /**
+   * Sets the Interrupt attribute of the KeyPress object
+   *
+   * @param newInterrupt The new Interrupt value
+   */
+  private void setInterrupt(Interrupt newInterrupt)
+  {
+    interrupt = newInterrupt;
+  }
+}
+
