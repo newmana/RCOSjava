7c086b005889e771b19b02f0b202a0951fc04d6a
newmana <>
2002-08-26 13:52:52 +0000

    Don't clone.

--- src/java/org/rcosjava/hardware/cpu/Context.java
+++ src/java/org/rcosjava/hardware/cpu/Context.java
@@ -131,7 +131,7 @@ public class Context implements Cloneable, Serializable
    */
   public Instruction getInstructionRegister()
   {
-    return (Instruction) instructionRegister.clone();
+    return instructionRegister;
   }
 
   /**
