022219256705f52a14f4d3e0aa43ecb408331696
newmana <>
2002-11-06 10:58:37 +0000

    Fixed up to use Swing and removed pictures.

--- src/java/org/rcosjava/software/animator/about/AboutFrame.java
+++ src/java/org/rcosjava/software/animator/about/AboutFrame.java
@@ -6,7 +6,6 @@ import javax.swing.*;
 import org.rcosjava.RCOS;
 import org.rcosjava.software.animator.RCOSFrame;
 import org.rcosjava.software.animator.support.GraphicButton;
-import org.rcosjava.software.animator.support.NewLabel;
 
 /**
  * To display the authors in all their glory.
@@ -22,20 +21,20 @@ import org.rcosjava.software.animator.support.NewLabel;
 public class AboutFrame extends RCOSFrame
 {
   /**
-   * Description of the Field
+   * Images of authors.
    */
-  private Image[] myImages = new Image[RCOS.NUMBER_OF_PEOPLE];
+  private Image[] myImages;
 
   /**
    * Constructor for the AboutFrame object
    *
-   * @param x Description of Parameter
-   * @param y Description of Parameter
-   * @param images Description of Parameter
+   * @param x width of the frame.
+   * @param y height of the frame.
+   * @param images pictures of the authors.
    */
   public AboutFrame(int x, int y, ImageIcon images[])
   {
-    setTitle("About RCOS");
+    setTitle("About RCOSjava");
     myImages = new Image[images.length];
     for (int index = 0; index < images.length; index++)
     {
@@ -45,120 +44,143 @@ public class AboutFrame extends RCOSFrame
   }
 
   /**
-   * Description of the Method
+   * Creates a panel for the title, a panel for the authors and a panel for the
+   * close button and adds them to the content pane.
    *
-   * @param c Description of Parameter
+   * @param c the parent component.
    */
-  public void setupLayout(Component c)
+  public void setupLayout(JComponent c)
   {
-    Panel pMain = new Panel();
-    Panel panelClose = new Panel();
-    GraphicButton tmpGButton;
-    NewLabel lTmpLabel;
+    super.setupLayout(c);
+    JPanel titlePanel = new JPanel();
+    titlePanel.setBackground(defaultBgColour);
+    titlePanel.setForeground(defaultFgColour);
 
+    JPanel contentPanel = new JPanel();
+    contentPanel.setBackground(defaultBgColour);
+    contentPanel.setForeground(defaultFgColour);
     GridBagConstraints constraints = new GridBagConstraints();
     GridBagLayout gridBag = new GridBagLayout();
-
     constraints.gridwidth = 1;
     constraints.gridheight = 1;
     constraints.weighty = 1;
     constraints.weightx = 1;
     constraints.insets = new Insets(3, 3, 3, 3);
+    constraints.anchor = GridBagConstraints.WEST;
+    constraints.gridwidth = GridBagConstraints.REMAINDER;
+    contentPanel.setLayout(gridBag);
+
+    JPanel closePanel = new JPanel();
+    closePanel.setBackground(defaultBgColour);
+    closePanel.setForeground(defaultFgColour);
+
+    JLabel tmpLabel;
+
+    tmpLabel = new JLabel("About RCOSjava");
+    tmpLabel.setFont(titleFont);
+    tmpLabel.setBackground(defaultBgColour);
+    tmpLabel.setForeground(defaultFgColour);
+    titlePanel.add(tmpLabel);
+
+    JPanel tmpPanel = new JPanel();
+    tmpPanel.setBackground(defaultBgColour);
+    tmpPanel.setForeground(defaultFgColour);
+    tmpLabel = new JLabel("Version 0.5");
+    tmpLabel.setBackground(defaultBgColour);
+    tmpLabel.setForeground(defaultFgColour);
+    tmpPanel.add(tmpLabel);
+    gridBag.setConstraints(tmpPanel, constraints);
+    contentPanel.add(tmpPanel);
+
+    tmpPanel = new JPanel();
+    tmpPanel.setBackground(defaultBgColour);
+    tmpPanel.setForeground(defaultFgColour);
+    tmpLabel = new JLabel("Licensed under the GPL (GNU Public Licence)");
+    tmpLabel.setBackground(defaultBgColour);
+    tmpLabel.setForeground(defaultFgColour);
+    tmpPanel.add(tmpLabel);
+    gridBag.setConstraints(tmpPanel, constraints);
+    contentPanel.add(tmpPanel);
+
+    tmpPanel = new JPanel();
+    tmpPanel.setBackground(defaultBgColour);
+    tmpPanel.setForeground(defaultFgColour);
+    tmpLabel = new JLabel("Copyright 1997-2002");
+    tmpLabel.setBackground(defaultBgColour);
+    tmpLabel.setForeground(defaultFgColour);
+    tmpPanel.add(tmpLabel);
+    gridBag.setConstraints(tmpPanel, constraints);
+    contentPanel.add(tmpPanel);
+
+    tmpPanel = new JPanel();
+    tmpPanel.setBackground(defaultBgColour);
+    tmpPanel.setForeground(defaultFgColour);
+    tmpLabel = new JLabel("Current Contributors:");
+    tmpLabel.setBackground(defaultBgColour);
+    tmpLabel.setForeground(defaultFgColour);
+    tmpPanel.add(tmpLabel);
+    gridBag.setConstraints(tmpPanel, constraints);
+    contentPanel.add(tmpPanel);
+
+    tmpPanel = new JPanel();
+    tmpPanel.setBackground(defaultBgColour);
+    tmpPanel.setForeground(defaultFgColour);
+    tmpLabel = new JLabel("Andrew Newman");
+    tmpLabel.setBackground(defaultBgColour);
+    tmpLabel.setForeground(defaultFgColour);
+    tmpLabel.setFont(defaultFont);
+    tmpPanel.add(tmpLabel);
     constraints.anchor = GridBagConstraints.CENTER;
-    pMain.setLayout(gridBag);
+    gridBag.setConstraints(tmpPanel, constraints);
+    contentPanel.add(tmpPanel);
 
-    constraints.gridwidth = GridBagConstraints.REMAINDER;
-    lTmpLabel = new NewLabel("RCOS.java Team.", titleFont);
-    gridBag.setConstraints(lTmpLabel, constraints);
-    pMain.add(lTmpLabel);
+    tmpPanel = new JPanel();
+    tmpPanel.setBackground(defaultBgColour);
+    tmpPanel.setForeground(defaultFgColour);
+    tmpLabel = new JLabel("Original Contributors:");
+    tmpLabel.setBackground(defaultBgColour);
+    tmpLabel.setForeground(defaultFgColour);
+    tmpPanel.add(tmpLabel);
+    constraints.anchor = GridBagConstraints.WEST;
+    gridBag.setConstraints(tmpPanel, constraints);
+    contentPanel.add(tmpPanel);
 
-    constraints.gridwidth = 1;
-    tmpGButton = new GraphicButton(myImages[0], myImages[0], "David Jones",
-        defaultFont, defaultFgColour, false, false);
-    gridBag.setConstraints(tmpGButton, constraints);
-    pMain.add(tmpGButton);
+    tmpPanel = new JPanel(new GridLayout(4,1));
+    tmpPanel.setBackground(defaultBgColour);
+    tmpPanel.setForeground(defaultFgColour);
+    tmpLabel = new JLabel("Brett Carter");
+    tmpLabel.setBackground(defaultBgColour);
+    tmpLabel.setForeground(defaultFgColour);
+    tmpLabel.setFont(defaultFont);
+    tmpPanel.add(tmpLabel);
+    tmpLabel = new JLabel("Bruce Jamieson");
+    tmpLabel.setBackground(defaultBgColour);
+    tmpLabel.setForeground(defaultFgColour);
+    tmpLabel.setFont(defaultFont);
+    tmpPanel.add(tmpLabel);
+    tmpLabel = new JLabel("David Jones");
+    tmpLabel.setBackground(defaultBgColour);
+    tmpLabel.setForeground(defaultFgColour);
+    tmpLabel.setFont(defaultFont);
+    tmpPanel.add(tmpLabel);
+    tmpLabel = new JLabel("Andrew Newman");
+    tmpLabel.setBackground(defaultBgColour);
+    tmpLabel.setForeground(defaultFgColour);
+    tmpLabel.setFont(defaultFont);
+    tmpPanel.add(tmpLabel);
+    constraints.anchor = GridBagConstraints.CENTER;
+    gridBag.setConstraints(tmpPanel, constraints);
+    contentPanel.add(tmpPanel);
 
-    Panel panel1 = new Panel();
-
-    panel1.setLayout(new GridLayout(5, 1));
-
-    panel1.add(new NewLabel("Name: David Jones.   ", defaultFont, false));
-    panel1.add(new NewLabel("Responsible for:     ", defaultFont, false));
-    panel1.add(new NewLabel("- Kernel,            ", defaultFont, false));
-    panel1.add(new NewLabel("- CPU, and           ", defaultFont, false));
-    panel1.add(new NewLabel("- Hardware Terminals.", defaultFont, false));
-
-    gridBag.setConstraints(panel1, constraints);
-    pMain.add(panel1);
-
-    tmpGButton = new GraphicButton(myImages[1], myImages[1], "Brett Carter",
-        defaultFont, buttonColour, false, false);
-
-    gridBag.setConstraints(tmpGButton, constraints);
-    pMain.add(tmpGButton);
-
-    Panel panel2 = new Panel();
-
-    panel2.setLayout(new GridLayout(5, 1));
-
-    panel2.add(new NewLabel("Name: Brett Carter.  ", defaultFont, false));
-    panel2.add(new NewLabel("Responsible for:     ", defaultFont, false));
-    panel2.add(new NewLabel("- Disk Schedular,    ", defaultFont, false));
-    panel2.add(new NewLabel("- File System, and   ", defaultFont, false));
-    panel2.add(new NewLabel("- File Server.       ", defaultFont, false));
-
-    constraints.gridwidth = GridBagConstraints.REMAINDER;
-    gridBag.setConstraints(panel2, constraints);
-    pMain.add(panel2);
-
-    tmpGButton = new GraphicButton(myImages[2], myImages[2], "Bruce Jamieson",
-        defaultFont, buttonColour, false, false);
-
-    constraints.gridwidth = 1;
-    gridBag.setConstraints(tmpGButton, constraints);
-    pMain.add(tmpGButton);
-
-    Panel panel3 = new Panel();
-
-    panel3.setLayout(new GridLayout(5, 1));
-
-    panel3.add(new NewLabel("Name: Bruce Jamieson.", defaultFont, false));
-    panel3.add(new NewLabel("Responsible for:     ", defaultFont, false));
-    panel3.add(new NewLabel("- Memory Manager,    ", defaultFont, false));
-    panel3.add(new NewLabel("- IPC Manager, and   ", defaultFont, false));
-    panel3.add(new NewLabel("- Post Office.       ", defaultFont, false));
-
-    gridBag.setConstraints(panel3, constraints);
-    pMain.add(panel3);
-
-    tmpGButton = new GraphicButton(myImages[3], myImages[3], "Andrew Newman",
-        defaultFont, buttonColour, false, false);
-
-    gridBag.setConstraints(tmpGButton, constraints);
-    pMain.add(tmpGButton);
-
-    Panel panel4 = new Panel();
-
-    panel4.setLayout(new GridLayout(6, 1));
-    panel4.add(new NewLabel("Name: Andrew Newman. ", defaultFont, false));
-    panel4.add(new NewLabel("Responsible for:     ", defaultFont, false));
-    panel4.add(new NewLabel("- User Interface,    ", defaultFont, false));
-    panel4.add(new NewLabel("- Combinig RCOS elements,", defaultFont, false));
-    panel4.add(new NewLabel("- Fixing bugs, and", defaultFont, false));
-    panel4.add(new NewLabel("- Utility Objects.    ", defaultFont, false));
-
-    constraints.gridwidth = GridBagConstraints.REMAINDER;
-    gridBag.setConstraints(panel4, constraints);
-    pMain.add(panel4);
-
-    panelClose.setLayout(new FlowLayout(FlowLayout.RIGHT));
+    closePanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+    closePanel.setBackground(defaultBgColour);
+    closePanel.setForeground(defaultFgColour);
     tmpButton = new JButton("Close");
-    panelClose.add(tmpButton);
+    closePanel.add(tmpButton);
     tmpButton.addMouseListener(new RCOSFrame.CloseAnimator());
 
-    getContentPane().add("Center", pMain);
-    getContentPane().add("South", panelClose);
-
-    repaint();
+    getContentPane().add("North", titlePanel);
+    getContentPane().add("Center", contentPanel);
+    getContentPane().add("South", closePanel);
   }
 }
