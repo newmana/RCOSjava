8dcbfdab58e468ecabcef1985a67599a7a577956
newmana <>
2003-02-16 12:31:12 +0000

    Updated javadoc.

--- src/java/org/rcosjava/hardware/disk/Disk.java
+++ src/java/org/rcosjava/hardware/disk/Disk.java
@@ -1,1 +1,155 @@
-package org.rcosjava.hardware.disk;/** * A Disk is the physical device which reads and writes data to a permanent * storage medium. The actual implementation of the properties of the disk <P> * * * * @author Andrew Newman. * @created 10th of August 1999 * @version 1.00 $Date$ */public interface Disk{  /**   * Returns the total number of tracks that the disk stores.   *   * @return The Tracks value   */  public int getTracks();  /**   * Returns the total number of sectos per track (assumed constant).   *   * @return The SectorsPerTrack value   */  public int getSectorsPerTrack();  /**   * Returns the number of bytes stored on each sectory.   *   * @return The BytesPerSector value   */  public int getBytesPerSector();  /**   * The time taken (in ms) to traverse a cyclinder.   *   * @return The DiskLatency value   */  public float getDiskLatency();  /**   * The time taken (in ms) to get the drive ready in a read process.   *   * @return The StartUpTime value   */  public int getStartUpTime();  /**   * The speed at which the media travels underneath the read head at.   *   * @return The RPM value   */  public int getRPM();  /**   * Calculates the whole number of sectors to read.   *   * @param bytesToRead the total number of bytes that are to be used to   *      calculate the number of sectors.   * @return Description of the Returned Value   */  public int calcSectorsToRead(int bytesToRead);  /**   * Calculates the whole number of tracks to read.   *   * @param bytesToRead the total number of bytes that are to be used to   *      calculate the number of tracks.   * @return Description of the Returned Value   */  public int calcTracksToRead(int bytesToRead);  /**   * Calculates the seek time based on the physical properties of the disk.   *   * @param tracks the number of tracks to read.   * @return Description of the Returned Value   */  public float calcSeekTime(int tracks);  /**   * Calculate the rotational delay of the disk or rather the time it takes for   * the correct position of the disk to travel under the read head.   *   * @return Description of the Returned Value   */  public float calcAverageRotationalDelay();  /**   * Calculate the transfer speed that the data is taken from the disk. This is   * usually a function of the rotation speed of the disk.   *   * @param bytes Description of Parameter   * @return Description of the Returned Value   */  public float calcTransferTime(int bytes);  /**   * Calculates the time taken to read a number of bytes from sector(s) assuming   * that are stored contiguously. This only takes into consideration the raw   * time of reading sectors (not track movement).   *   * @param bytesToRead the number of bytes to read from the sector(s).   * @return Description of the Returned Value   */  public float calcSuccessiveSectorRead(int bytesToRead);  /**   * Calculates the time taken to read a number of bytes from track(s) assumin   * that they are stored contiguously. This does not include the time taken to   * read the sector data.   *   * @param bytesToRead Description of Parameter   * @return Description of the Returned Value   */  public float calcSuccessiveTrackRead(int bytesToRead);  /**   * Calculates the total time taken assuming that the data is contiguously   * stored on the disk.   *   * @param bytesToRead Description of Parameter   * @return Description of the Returned Value   */  public float calcSequentialAccessTime(int bytesToRead);  /**   * Calculates the total time taken assuming the data is randomly stored on the   * disk media.   *   * @param bytesToRead Description of Parameter   * @return Description of the Returned Value   */  public float calcRandomAccessTime(int bytesToRead);  /**   * Physical access to the media of the disk. Read block of data   * (track/sector).   *   * @param track the disk section to read from.   * @param sector the disk section to read from.   * @return the value stored on the disk.   */  public byte[] readSector(int track, int sector);  /**   * Physical access to the media of the disk. Write a block of data to the   * disk.   *   * @param track the disk section to write to.   * @param sector the disk section to write to.   * @param data the data to write to the disk.   */  public void writeSector(int track, int sector, byte[] data);}
+package org.rcosjava.hardware.disk;
+
+/**
+ * A Disk is the physical device which reads and writes data to a permanent
+ * storage medium. The actual implementation of the properties of the disk.
+ * <P>
+ * @author Andrew Newman.
+ * @created 10th of August 1999
+ * @version 1.00 $Date$
+ */
+public interface Disk
+{
+  /**
+   * Returns the total number of tracks that the disk stores.
+   *
+   * @return The Tracks value
+   */
+  public int getTracks();
+
+  /**
+   * Returns the total number of sectos per track (assumed constant).
+   *
+   * @return The SectorsPerTrack value
+   */
+  public int getSectorsPerTrack();
+
+  /**
+   * Returns the number of bytes stored on each sectory.
+   *
+   * @return The BytesPerSector value
+   */
+  public int getBytesPerSector();
+
+  /**
+   * The time taken (in ms) to traverse a cyclinder.
+   *
+   * @return The DiskLatency value
+   */
+  public float getDiskLatency();
+
+  /**
+   * The time taken (in ms) to get the drive ready in a read process.
+   *
+   * @return The StartUpTime value
+   */
+  public int getStartUpTime();
+
+  /**
+   * The speed at which the media travels underneath the read head at.
+   *
+   * @return The RPM value
+   */
+  public int getRPM();
+
+  /**
+   * Calculates the whole number of sectors to read.
+   *
+   * @param bytesToRead the total number of bytes that are to be used to
+   *      calculate the number of sectors.
+   * @return Description of the Returned Value
+   */
+  public int calcSectorsToRead(int bytesToRead);
+
+  /**
+   * Calculates the whole number of tracks to read.
+   *
+   * @param bytesToRead the total number of bytes that are to be used to
+   *      calculate the number of tracks.
+   * @return Description of the Returned Value
+   */
+  public int calcTracksToRead(int bytesToRead);
+
+  /**
+   * Calculates the seek time based on the physical properties of the disk.
+   *
+   * @param tracks the number of tracks to read.
+   * @return Description of the Returned Value
+   */
+  public float calcSeekTime(int tracks);
+
+  /**
+   * Calculate the rotational delay of the disk or rather the time it takes for
+   * the correct position of the disk to travel under the read head.
+   *
+   * @return Description of the Returned Value
+   */
+  public float calcAverageRotationalDelay();
+
+  /**
+   * Calculate the transfer speed that the data is taken from the disk. This is
+   * usually a function of the rotation speed of the disk.
+   *
+   * @param bytes Description of Parameter
+   * @return Description of the Returned Value
+   */
+  public float calcTransferTime(int bytes);
+
+  /**
+   * Calculates the time taken to read a number of bytes from sector(s) assuming
+   * that are stored contiguously. This only takes into consideration the raw
+   * time of reading sectors (not track movement).
+   *
+   * @param bytesToRead the number of bytes to read from the sector(s).
+   * @return Description of the Returned Value
+   */
+  public float calcSuccessiveSectorRead(int bytesToRead);
+
+  /**
+   * Calculates the time taken to read a number of bytes from track(s) assumin
+   * that they are stored contiguously. This does not include the time taken to
+   * read the sector data.
+   *
+   * @param bytesToRead Description of Parameter
+   * @return Description of the Returned Value
+   */
+  public float calcSuccessiveTrackRead(int bytesToRead);
+
+  /**
+   * Calculates the total time taken assuming that the data is contiguously
+   * stored on the disk.
+   *
+   * @param bytesToRead Description of Parameter
+   * @return Description of the Returned Value
+   */
+  public float calcSequentialAccessTime(int bytesToRead);
+
+  /**
+   * Calculates the total time taken assuming the data is randomly stored on the
+   * disk media.
+   *
+   * @param bytesToRead Description of Parameter
+   * @return Description of the Returned Value
+   */
+  public float calcRandomAccessTime(int bytesToRead);
+
+  /**
+   * Physical access to the media of the disk. Read block of data
+   * (track/sector).
+   *
+   * @param track the disk section to read from.
+   * @param sector the disk section to read from.
+   * @return the value stored on the disk.
+   */
+  public byte[] readSector(int track, int sector);
+
+  /**
+   * Physical access to the media of the disk. Write a block of data to the
+   * disk.
+   *
+   * @param track the disk section to write to.
+   * @param sector the disk section to write to.
+   * @param data the data to write to the disk.
+   */
+  public void writeSector(int track, int sector, byte[] data);
+}
--- src/java/org/rcosjava/hardware/disk/SimpleDisk.java
+++ src/java/org/rcosjava/hardware/disk/SimpleDisk.java
@@ -1,1 +1,280 @@
-package org.rcosjava.hardware.disk;/** * General simulation of a disk (floppy or hard drive). Extend this to implement * specific implementations. <P> * * * * @author Andrew Newman. * @created 10th of August 1999 * @see org.rcosjava.hardware.disk.Disk * @version 1.00 $Date$ */public class SimpleDisk implements Disk{  /**   * Description of the Field   */  private int tracks;  /**   * Description of the Field   */  private int sectorsPerTrack;  /**   * Description of the Field   */  private int bytesPerSector;  /**   * Description of the Field   */  private float diskLatency;  /**   * Description of the Field   */  private int startupTime, rpm;  /**   * Description of the Field   */  private byte[] disk;  /**   * Constructor for the SimpleDisk object   *   * @param newTracks Description of Parameter   * @param newSectorsPerTrack Description of Parameter   * @param newBytesPerSector Description of Parameter   * @param newDiskLatency Description of Parameter   * @param newStartupTime Description of Parameter   * @param newRPM Description of Parameter   */  public SimpleDisk(int newTracks, int newSectorsPerTrack,      int newBytesPerSector, float newDiskLatency, int newStartupTime,      int newRPM)  {    tracks = newTracks;    sectorsPerTrack = newSectorsPerTrack;    bytesPerSector = newBytesPerSector;    diskLatency = newDiskLatency;    startupTime = newStartupTime;    rpm = newRPM;    disk = new byte[newTracks * newSectorsPerTrack * bytesPerSector];  }  /**   * Gets the Tracks attribute of the SimpleDisk object   *   * @return The Tracks value   */  public int getTracks()  {    return tracks;  }  /**   * Gets the SectorsPerTrack attribute of the SimpleDisk object   *   * @return The SectorsPerTrack value   */  public int getSectorsPerTrack()  {    return sectorsPerTrack;  }  /**   * Gets the BytesPerSector attribute of the SimpleDisk object   *   * @return The BytesPerSector value   */  public int getBytesPerSector()  {    return bytesPerSector;  }  /**   * Gets the DiskLatency attribute of the SimpleDisk object   *   * @return The DiskLatency value   */  public float getDiskLatency()  {    return diskLatency;  }  /**   * Gets the StartUpTime attribute of the SimpleDisk object   *   * @return The StartUpTime value   */  public int getStartUpTime()  {    return startupTime;  }  /**   * Gets the RPM attribute of the SimpleDisk object   *   * @return The RPM value   */  public int getRPM()  {    return rpm;  }  /**   * Description of the Method   *   * @param noBytesToRead Description of Parameter   * @return Description of the Returned Value   */  public int calcSectorsToRead(int noBytesToRead)  {    return ((int) Math.ceil(noBytesToRead / getBytesPerSector()));  }  /**   * Description of the Method   *   * @param noBytesToRead Description of Parameter   * @return Description of the Returned Value   */  public int calcTracksToRead(int noBytesToRead)  {    return ((int) Math.ceil(noBytesToRead /        (getSectorsPerTrack() * getBytesPerSector())));  }  //Linear approximation of seek time  /**   * Description of the Method   *   * @param noTracks Description of Parameter   * @return Description of the Returned Value   */  public float calcSeekTime(int noTracks)  {    return (getDiskLatency() * noTracks + getStartUpTime());  }  /**   * Description of the Method   *   * @return Description of the Returned Value   */  public float calcAverageRotationalDelay()  {    return (60000 / rpm) / 2;  }  //Time it takes in ms to transfer iNoBytes  /**   * Description of the Method   *   * @param noBytes Description of Parameter   * @return Description of the Returned Value   */  public float calcTransferTime(int noBytes)  {    return (noBytes /        ((rpm / (60 * 1000)) * (getSectorsPerTrack() * getBytesPerSector())));  }  /**   * Description of the Method   *   * @param noBytesToRead Description of Parameter   * @return Description of the Returned Value   */  public float calcSuccessiveSectorRead(int noBytesToRead)  {    return (calcSeekTime(calcTracksToRead(noBytesToRead)) +        calcAverageRotationalDelay() +        calcTransferTime(getBytesPerSector()));  }  /**   * Description of the Method   *   * @param noBytesToRead Description of Parameter   * @return Description of the Returned Value   */  public float calcSuccessiveTrackRead(int noBytesToRead)  {    return (calcAverageRotationalDelay() + calcTransferTime(noBytesToRead));  }  // Assumes the data is stored as compactly as possible on the  // disk and there's no seek time between tracks.  /**   * Description of the Method   *   * @param noBytesToRead Description of Parameter   * @return Description of the Returned Value   */  public float calcSequentialAccessTime(int noBytesToRead)  {    return (calcSuccessiveTrackRead(noBytesToRead) *        calcTracksToRead(noBytesToRead) +        calcSeekTime(calcTracksToRead(noBytesToRead)));  }  // Assume the data is stored randomly on the disk.  /**   * Description of the Method   *   * @param noBytesToRead Description of Parameter   * @return Description of the Returned Value   */  public float calcRandomAccessTime(int noBytesToRead)  {    return (calcSuccessiveSectorRead(noBytesToRead) *        calcSectorsToRead(noBytesToRead));  }  /**   * Description of the Method   *   * @param track Description of Parameter   * @param sector Description of Parameter   * @return Description of the Returned Value   */  public byte[] readSector(int track, int sector)  {    int offset = calcOffset(track, sector);    byte[] returnData = new byte[getBytesPerSector()];    for (int count = 0; count < getBytesPerSector(); count++)    {      returnData[count] = disk[offset + count];    }    return returnData;  }  /**   * Description of the Method   *   * @param track Description of Parameter   * @param sector Description of Parameter   * @param data Description of Parameter   */  public void writeSector(int track, int sector, byte[] data)  {    int offset = calcOffset(track, sector);    for (int count = 0; count < getBytesPerSector(); count++)    {      disk[offset + count] = data[count];    }  }  /**   * Description of the Method   *   * @param track Description of Parameter   * @param sector Description of Parameter   * @return Description of the Returned Value   */  private int calcOffset(int track, int sector)  {    return (track * getSectorsPerTrack() * getBytesPerSector()) +        (sector * getBytesPerSector());  }}
+package org.rcosjava.hardware.disk;
+
+/**
+ * General simulation of a disk (floppy or hard drive). Extend this to implement
+ * specific implementations.
+ * <P>
+ * @author Andrew Newman.
+ * @created 10th of August 1999
+ * @see org.rcosjava.hardware.disk.Disk
+ * @version 1.00 $Date$
+ */
+public class SimpleDisk implements Disk
+{
+  /**
+   * Description of the Field
+   */
+  private int tracks;
+  /**
+   * Description of the Field
+   */
+  private int sectorsPerTrack;
+  /**
+   * Description of the Field
+   */
+  private int bytesPerSector;
+  /**
+   * Description of the Field
+   */
+  private float diskLatency;
+  /**
+   * Description of the Field
+   */
+  private int startupTime, rpm;
+  /**
+   * Description of the Field
+   */
+  private byte[] disk;
+
+  /**
+   * Constructor for the SimpleDisk object
+   *
+   * @param newTracks Description of Parameter
+   * @param newSectorsPerTrack Description of Parameter
+   * @param newBytesPerSector Description of Parameter
+   * @param newDiskLatency Description of Parameter
+   * @param newStartupTime Description of Parameter
+   * @param newRPM Description of Parameter
+   */
+  public SimpleDisk(int newTracks, int newSectorsPerTrack,
+      int newBytesPerSector, float newDiskLatency, int newStartupTime,
+      int newRPM)
+  {
+    tracks = newTracks;
+    sectorsPerTrack = newSectorsPerTrack;
+    bytesPerSector = newBytesPerSector;
+    diskLatency = newDiskLatency;
+    startupTime = newStartupTime;
+    rpm = newRPM;
+    disk = new byte[newTracks * newSectorsPerTrack * bytesPerSector];
+  }
+
+  /**
+   * Gets the Tracks attribute of the SimpleDisk object
+   *
+   * @return The Tracks value
+   */
+  public int getTracks()
+  {
+    return tracks;
+  }
+
+  /**
+   * Gets the SectorsPerTrack attribute of the SimpleDisk object
+   *
+   * @return The SectorsPerTrack value
+   */
+  public int getSectorsPerTrack()
+  {
+    return sectorsPerTrack;
+  }
+
+  /**
+   * Gets the BytesPerSector attribute of the SimpleDisk object
+   *
+   * @return The BytesPerSector value
+   */
+  public int getBytesPerSector()
+  {
+    return bytesPerSector;
+  }
+
+  /**
+   * Gets the DiskLatency attribute of the SimpleDisk object
+   *
+   * @return The DiskLatency value
+   */
+  public float getDiskLatency()
+  {
+    return diskLatency;
+  }
+
+  /**
+   * Gets the StartUpTime attribute of the SimpleDisk object
+   *
+   * @return The StartUpTime value
+   */
+  public int getStartUpTime()
+  {
+    return startupTime;
+  }
+
+  /**
+   * Gets the RPM attribute of the SimpleDisk object
+   *
+   * @return The RPM value
+   */
+  public int getRPM()
+  {
+    return rpm;
+  }
+
+  /**
+   * Description of the Method
+   *
+   * @param noBytesToRead Description of Parameter
+   * @return Description of the Returned Value
+   */
+  public int calcSectorsToRead(int noBytesToRead)
+  {
+    return ((int) Math.ceil(noBytesToRead / getBytesPerSector()));
+  }
+
+  /**
+   * Description of the Method
+   *
+   * @param noBytesToRead Description of Parameter
+   * @return Description of the Returned Value
+   */
+  public int calcTracksToRead(int noBytesToRead)
+  {
+    return ((int) Math.ceil(noBytesToRead /
+        (getSectorsPerTrack() * getBytesPerSector())));
+  }
+
+  //Linear approximation of seek time
+  /**
+   * Description of the Method
+   *
+   * @param noTracks Description of Parameter
+   * @return Description of the Returned Value
+   */
+  public float calcSeekTime(int noTracks)
+  {
+    return (getDiskLatency() * noTracks + getStartUpTime());
+  }
+
+  /**
+   * Description of the Method
+   *
+   * @return Description of the Returned Value
+   */
+  public float calcAverageRotationalDelay()
+  {
+    return (60000 / rpm) / 2;
+  }
+
+  //Time it takes in ms to transfer iNoBytes
+  /**
+   * Description of the Method
+   *
+   * @param noBytes Description of Parameter
+   * @return Description of the Returned Value
+   */
+  public float calcTransferTime(int noBytes)
+  {
+    return (noBytes /
+        ((rpm / (60 * 1000)) * (getSectorsPerTrack() * getBytesPerSector())));
+  }
+
+  /**
+   * Description of the Method
+   *
+   * @param noBytesToRead Description of Parameter
+   * @return Description of the Returned Value
+   */
+  public float calcSuccessiveSectorRead(int noBytesToRead)
+  {
+    return (calcSeekTime(calcTracksToRead(noBytesToRead)) +
+        calcAverageRotationalDelay() +
+        calcTransferTime(getBytesPerSector()));
+  }
+
+  /**
+   * Description of the Method
+   *
+   * @param noBytesToRead Description of Parameter
+   * @return Description of the Returned Value
+   */
+  public float calcSuccessiveTrackRead(int noBytesToRead)
+  {
+    return (calcAverageRotationalDelay() + calcTransferTime(noBytesToRead));
+  }
+
+  // Assumes the data is stored as compactly as possible on the
+  // disk and there's no seek time between tracks.
+  /**
+   * Description of the Method
+   *
+   * @param noBytesToRead Description of Parameter
+   * @return Description of the Returned Value
+   */
+  public float calcSequentialAccessTime(int noBytesToRead)
+  {
+    return (calcSuccessiveTrackRead(noBytesToRead) *
+        calcTracksToRead(noBytesToRead) +
+        calcSeekTime(calcTracksToRead(noBytesToRead)));
+  }
+
+  // Assume the data is stored randomly on the disk.
+  /**
+   * Description of the Method
+   *
+   * @param noBytesToRead Description of Parameter
+   * @return Description of the Returned Value
+   */
+  public float calcRandomAccessTime(int noBytesToRead)
+  {
+    return (calcSuccessiveSectorRead(noBytesToRead) *
+        calcSectorsToRead(noBytesToRead));
+  }
+
+  /**
+   * Description of the Method
+   *
+   * @param track Description of Parameter
+   * @param sector Description of Parameter
+   * @return Description of the Returned Value
+   */
+  public byte[] readSector(int track, int sector)
+  {
+    int offset = calcOffset(track, sector);
+    byte[] returnData = new byte[getBytesPerSector()];
+
+    for (int count = 0; count < getBytesPerSector(); count++)
+    {
+      returnData[count] = disk[offset + count];
+    }
+    return returnData;
+  }
+
+  /**
+   * Description of the Method
+   *
+   * @param track Description of Parameter
+   * @param sector Description of Parameter
+   * @param data Description of Parameter
+   */
+  public void writeSector(int track, int sector, byte[] data)
+  {
+    int offset = calcOffset(track, sector);
+
+    for (int count = 0; count < getBytesPerSector(); count++)
+    {
+      disk[offset + count] = data[count];
+    }
+  }
+
+  /**
+   * Description of the Method
+   *
+   * @param track Description of Parameter
+   * @param sector Description of Parameter
+   * @return Description of the Returned Value
+   */
+  private int calcOffset(int track, int sector)
+  {
+    return (track * getSectorsPerTrack() * getBytesPerSector()) +
+        (sector * getBytesPerSector());
+  }
+}
