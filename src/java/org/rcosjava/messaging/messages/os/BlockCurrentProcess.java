97c21dcf80f6f20b241541442b6c58f0a84c0b64
newmana <>
2003-07-06 07:59:37 +0000

    Added decrementing program counter and javadocced.

--- src/java/org/rcosjava/messaging/messages/os/BlockCurrentProcess.java
+++ src/java/org/rcosjava/messaging/messages/os/BlockCurrentProcess.java
@@ -13,23 +13,32 @@ import org.rcosjava.software.kernel.Kernel;
 public class BlockCurrentProcess extends OSMessageAdapter
 {
   /**
-   * Constructor for the BlockCurrentProcess object
-   *
-   * @param theSource Description of Parameter
+   * Indicates whether to decrement the program counter (execute the same
+   * instruction after the process is blocked).
    */
-  public BlockCurrentProcess(OSMessageHandler theSource)
+  private boolean decrementProgramCounter = false;
+
+  /**
+   * Create a new BlockCurrentProcess message.
+   *
+   * @param theSource the sender of the message.
+   * @param newDecrementProgramCounter
+   */
+  public BlockCurrentProcess(OSMessageHandler theSource,
+      boolean newDecrementProgramCounter)
   {
     super(theSource);
+    decrementProgramCounter = newDecrementProgramCounter;
   }
 
   /**
-   * Description of the Method
+   * Call blockCurrentProcess on the Kernel.
    *
-   * @param theElement Description of Parameter
+   * @param theElement the kernel to block on.
    */
   public void doMessage(Kernel theElement)
   {
-    theElement.blockCurrentProcess();
+    theElement.blockCurrentProcess(decrementProgramCounter);
   }
 }
 
