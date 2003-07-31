1ed698fc7921461ba6946e9ff32a0d98b59352bb
newmana <>
2003-07-31 11:53:51 +0000

    First cut of CPM14 testing.

---
+++ src/java/org/rcosjava/test/FileSystemTest.java
@@ -0,0 +1,96 @@
+package org.rcosjava.test;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.rcosjava.messaging.postoffices.os.OSOffice;
+import org.rcosjava.software.filesystem.*;
+import org.rcosjava.software.filesystem.cpm14.*;
+
+/**
+ * Description of the Class
+ *
+ * @author administrator
+ * @created 28 April 2002
+ */
+public class FileSystemTest extends TestCase
+{
+  /**
+   * Constructor for the CPUTest object
+   *
+   * @param name Description of Parameter
+   */
+  public FileSystemTest(String name)
+  {
+    super(name);
+  }
+
+  /**
+   * A unit test suite for JUnit
+   *
+   * @return The test suite
+   */
+  public static Test suite()
+  {
+    TestSuite suite = new TestSuite();
+
+    suite.addTest(new FileSystemTest("testCPM14"));
+    return suite;
+  }
+
+  /**
+   * The main program for the CPUTest class
+   *
+   * @param args The command line arguments
+   */
+  public static void main(String[] args)
+  {
+    junit.textui.TestRunner.run(suite());
+  }
+
+  /**
+   * The JUnit setup method
+   */
+  public void setUp()
+  {
+  }
+
+  /**
+   * Tests the CPM14 file system implementation.
+   */
+  public void testCPM14()
+  {
+    try
+    {
+      FileSystem fs = new CPM14FileSystem();
+
+      // Mount a new drive.
+      String mountPoint = "C";
+      String deviceName = "DISK1";
+      fs.mount(mountPoint, deviceName);
+
+      // Get file system request.
+      int requestId = 0;
+      FileSystemReturnData data = fs.allocate(requestId, "C:test.pas");
+      int fileId = data.getReturnValue();
+
+      // Assume allocation is predictable
+      assertEquals("Request Id should be equal", 0, requestId);
+      assertEquals("File Id should be equal", 0, fileId);
+
+      // Increment the requestId each call.
+      requestId++;
+      data = fs.create(requestId, fileId);
+
+      assertEquals("Request Id should be equal", 1, requestId);
+      assertEquals("File Id should be equal", 0, fileId);
+
+      System.out.println(fs.dump("DIR", 0, 0));
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+    }
+  }
+}
