14972de4c3439031ec994efb75d93d75e3c0526a
newmana <>
2003-08-05 12:18:38 +0000

    Added testing of writing data.

--- src/java/org/rcosjava/test/FileSystemTest.java
+++ src/java/org/rcosjava/test/FileSystemTest.java
@@ -4,7 +4,13 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-import org.rcosjava.messaging.postoffices.os.OSOffice;
+import org.rcosjava.RCOS;
+import org.rcosjava.messaging.postoffices.os.*;
+import org.rcosjava.messaging.postoffices.universal.*;
+import org.rcosjava.messaging.messages.os.AddDiskRequest;
+import org.rcosjava.messaging.messages.os.OSMessageAdapter;
+import org.rcosjava.messaging.messages.universal.UniversalMessageAdapter;
+import org.rcosjava.software.disk.*;
 import org.rcosjava.software.filesystem.*;
 import org.rcosjava.software.filesystem.cpm14.*;
 
@@ -16,6 +22,8 @@ import org.rcosjava.software.filesystem.cpm14.*;
  */
 public class FileSystemTest extends TestCase
 {
+  private FileSystem fs;
+
   /**
    * Constructor for the CPUTest object
    *
@@ -63,7 +71,12 @@ public class FileSystemTest extends TestCase
   {
     try
     {
-      FileSystem fs = new CPM14FileSystem();
+      OSOffice postOffice = new OSOffice(RCOS.OS_POST_OFFICE_ID);
+      fs = new CPM14FileSystem(postOffice);
+      SimpleMessageHandler handler = new SimpleMessageHandler(postOffice);
+
+      // String to hold dump information.
+      String dump;
 
       // Mount a new drive.
       String mountPoint = "C";
@@ -77,6 +90,11 @@ public class FileSystemTest extends TestCase
       FileSystemReturnData data = fs.allocate(requestId, "C:test.pas");
       int fileId = data.getReturnValue();
 
+      // Ensure allocated correctly.
+      dump = fs.dumpFIDEntry(fileId);
+      assertEquals("The current file id entry should have the correct " +
+        "filename and mode", "C:test.pas,0", dump);
+
       // Assume allocation is predictable
       assertEquals("Request Id should be equal", 0, requestId);
       assertEquals("File Id should be equal", 0, fileId);
@@ -88,39 +106,68 @@ public class FileSystemTest extends TestCase
       // to zero.
       data = fs.create(requestId, fileId);
 
+      // Ensure created correctly.
+      dump = fs.dumpFIDEntry(fileId);
+      assertEquals("The current file id entry should have the correct " +
+        "filename and mode", "C:test.pas,2", dump);
+
       // Ensure that the file was created without error.
       assertEquals("Request Id should be equal", 1, requestId);
       assertEquals("File Id should be equal", 0, fileId);
 
       // Ensure that the first directory entry has been created correctly.
-      String dump = fs.dumpDirectoryEntry(0, 0);
-      System.out.println(dump);
+      dump = fs.dumpDirectoryEntry(0, 0);
       assertEquals("Directory request should have one file",
           "0 t e s t         p a s 0 0 0 0 \n0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 " +
           "\n", dump);
+
+      dump = fs.dumpFIDEntry(fileId);
+      assertEquals("The current file id entry should have the correct " +
+        "filename and mode", "C:test.pas,2", dump);
+
+      int index = 1;
 
       // Write to the file system some bytes.
       for (int counter = 0; counter < 4000; counter++)
       {
         requestId++;
         fs.write(requestId, fileId, (byte) 'b');
+
+        // Make sure we deliver the message to the listeners.
+        postOffice.deliverMessages();
       }
 
+      // Check the entry now has the correct number of records and data blocks
+      // allocated.
       dump = fs.dumpDirectoryEntry(0, 0);
-      System.out.println(dump);
-      dump = fs.dumpDirectoryEntry(0, 1);
-      System.out.println(dump);
+      assertEquals("Directory request should have one file, with 4 allocated " +
+        "sectors", "0 t e s t         p a s 0 0 0 31 \n2 3 4 5 0 0 0 0 0 0 0 " +
+        "0 0 0 0 0 \n", dump);
 
-      data = fs.close(requestId++, fileId);
-
-      dump = fs.dumpDirectoryEntry(0, 0);
-      System.out.println(dump);
-      dump = fs.dumpDirectoryEntry(0, 1);
-      System.out.println(dump);
+      dump = fs.dumpFIDEntry(fileId);
+      assertEquals("The current file id entry should have the correct " +
+        "filename and mode", "C:test.pas,2", dump);
     }
     catch (Exception e)
     {
       e.printStackTrace();
+    }
+  }
+
+  public class SimpleMessageHandler extends OSMessageRecorder
+  {
+    public SimpleMessageHandler(OSOffice newPostOffice)
+    {
+      super("SimpleMessageHandler", newPostOffice, null);
+    }
+
+    public void processMessage(OSMessageAdapter newMessage)
+    {
+      if (newMessage.getType() == "org.rcosjava.messaging.messages.os.AddDiskRequest")
+      {
+        AddDiskRequest msg = (AddDiskRequest) newMessage;
+        fs.flush(msg.getDiskRequest());
+      }
     }
   }
 }
