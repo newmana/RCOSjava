d8e2e7c79a3f72f4f576708ad9f5839ac3a16b94
newmana <>
2003-08-06 09:33:17 +0000

    New message used by file system.

---
+++ src/java/org/rcosjava/messaging/messages/os/AddDiskRequest.java
@@ -0,0 +1,52 @@
+package org.rcosjava.messaging.messages.os;
+
+import org.rcosjava.software.disk.DiskManager;
+import org.rcosjava.software.disk.DiskRequest;
+import org.rcosjava.messaging.postoffices.os.OSMessageHandler;
+
+/**
+ * Adds to the disk request to the disk manager.
+ * <P>
+ * @author Andrew Newman
+ * @created 28 March 1996
+ */
+public class AddDiskRequest extends OSMessageAdapter
+{
+  /**
+   * A request to add.
+   */
+  private DiskRequest request;
+
+  /**
+   * Create a new AddDiskRequest message.
+   *
+   * @param theSource the messaging handler that is sending the message.
+   * @param newRequest the request to add.
+   */
+  public AddDiskRequest(OSMessageHandler theSource, DiskRequest newRequest)
+  {
+    super(theSource);
+    request = newRequest;
+  }
+
+  /**
+   * Returns the disk request.
+   *
+   * @return the disk request.
+   */
+  public DiskRequest getDiskRequest()
+  {
+    return request;
+  }
+
+  /**
+   * Calls addInterrupt on the CPU via the Kernel.
+   *
+   * @param theElement the Kernel object to call.
+   */
+  public void doMessage(DiskManager theElement)
+  {
+    System.err.println("Doing addDiskRequest!");
+    theElement.addDiskRequest(getSource().getId(), request);
+  }
+}
